cde37af9ad3abb3f4b1bdc11b039090f2f25a474
Anthony Johnson <aj@ohess.org>
2021-10-20 15:30:38 -0700

    More happo config

--- .happo.js
+++ .happo.js
@@ -1,17 +1,12 @@
 const { RemoteBrowserTarget } = require('happo.io');
 
+const branch = process.env.CIRCLE_BRANCH;
+const url = 'https://readthedocs-static-dev.s3.us-east-2.amazonaws.com/qa/theme/' + branch + '/index.html';
+
 module.exports = {
-  project: "test-sphinx-theme",
+  project: 'test-sphinx-theme',
 
-  // apiKey: process.env.HAPPO_API_KEY,
-  //apiSecret: process.env.HAPPO_API_SECRET,
-
-  pages: [
-    { 
-        url: 'https://readthedocs-static-dev.s3.us-east-2.amazonaws.com/qa/theme/sphinx4-theme1-docutils17-html5/demo/demo.html' //,
-        // title: 'Sphinx 4, HTML 5'
-    },
-  ],
+  pages: [{ url: url, title: branch }],
 
   targets: {
     'chrome-desktop': new RemoteBrowserTarget('chrome', {
