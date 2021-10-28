52b24260be450385f99727be065c1e7f300b718e
Anthony Johnson <aj@ohess.org>
2021-10-27 19:40:43 -0700

    Tweak names

--- cypress/integration/nav_spec.js
+++ cypress/integration/nav_spec.js
@@ -1,11 +1,11 @@
-describe('Side navigation', function () {
+describe('Navigation', function () {
   before(() => {
     cy.visit('./docs/_build/html/index.html');
   });
   it('is visible', function () {
     cy.get('nav.wy-nav-side').happoScreenshot();
   });
-  it('has a TOC', () => {
+  it('expands', () => {
     cy.get('a.reference.current').find('span.toctree-expand').first().click();
     cy.get('nav.wy-nav-side').happoScreenshot();
   })
