f7fe198544d0451279d424d9b591fb40c615428c
Anthony Johnson <aj@ohess.org>
2021-10-28 12:04:06 -0700

    Try remote snapshots instead

--- tests/config/support/commands.js
+++ tests/config/support/commands.js
@@ -1,6 +1,10 @@
 import 'happo-cypress';
 import { configure } from 'happo-cypress';
 
+/* Disabling for now, this seems to cause issues with Happo targets. It was
+ * originally enabled to get stylesheets at Happo from our local sources. */
+/*
 configure({
   localSnapshots: true
 });
+*/
